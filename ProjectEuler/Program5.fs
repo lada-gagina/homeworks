fb96daa2b48f5962b1e038e25641fc301cca4e36
Lada Gagina <tsubame.desu@yahoo.com>
2013-04-08 05:57:18 +0400

    deleted excess files

--- ProjectEuler/Program5.fs
+++
@@ -1,9 +0,0 @@
-﻿//problem #5
-//Lada Gagina
-//(c)2013
-
-let rec gcd a b = if b = 0 then a else gcd b (a % b)
-let mutable lcm = 2520
-for i = 11 to 20 do
-    lcm <- lcm * i / gcd lcm i
-printfn "%A" lcm
