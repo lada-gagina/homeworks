7ad21fde9493ae0e5f7f962a409228918b88aef3
Lada Gagina <tsubame.desu@yahoo.com>
2013-04-29 14:21:44 +0400

    Update Problem2.fs
    changed

--- ProjectEuler/Problem2.fs
+++ ProjectEuler/Problem2.fs
@@ -6,7 +6,7 @@ let mutable sum = 0
 let mutable i = 1
 let mutable j = 2
 while j <= 4000000 do
-     if j % 2 = 0 then sum <- sum + j else sum <- sum + 0
+     if j % 2 = 0 then sum <- sum + j
      j <- i + j
      i <- j - i
 printfn "%A" sum
