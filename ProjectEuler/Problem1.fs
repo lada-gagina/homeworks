2f7fd19bcfb83af356edc4d89ea44379920e6076
Lada Gagina <tsubame.desu@yahoo.com>
2013-05-12 20:16:17 +0400

    All ProjectEuler problems were corrected

--- ProjectEuler/Problem1.fs
+++ ProjectEuler/Problem1.fs
@@ -1,4 +1,4 @@
-﻿﻿//The sum of all the multiples of 3 or 5 below 1000
+﻿//The sum of all the multiples of 3 or 5 below 1000
 //Lada Gagina
 //(c)2013
 
@@ -6,6 +6,6 @@ let mutable tempsum = 0
 let first = 3 
 let last = 999
 let sum =
-    for i in first..last do
+    for i in first .. last do
         if i % 3 = 0 || i % 5 = 0 then tempsum <- tempsum + i
 printfn "%A" tempsum
