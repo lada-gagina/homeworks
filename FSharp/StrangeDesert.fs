17b1a1e16e3c8d64dc2347ffe40db3080b785554
Lada Gagina <l4.swallow@gmail.com>
2013-10-27 14:09:50 +0400

    modified StrangeDesert.fs

--- FSharp/StrangeDesert.fs
+++ FSharp/StrangeDesert.fs
@@ -1,54 +1,62 @@
 ﻿open System
 
-type Creature(itsName:string, itsAge:int, itsEnergy)=
+type Creature(itsName:string, itsAge:int) =
     let mutable name = itsName
     let mutable age = itsAge
-    let mutable energy = itsEnergy
     let mutable alive = true
 
     member this.Alive = alive
+    member this.Age = age
+
     member this.Die() =
         if (this.Alive) then alive <- false
 
     member this.Grow() =
         age <- age + 1
 
-type Plant =
-    inherit Creature
+type Plant(itsName:string, itsAge:int, itsToxicity:bool) =
+    inherit Creature(itsName, itsAge)
 
-    val toxic : bool
-
-
-type Animal =
-    inherit Creature
-
-    val speed : int
-    val agility : int
+    let toxic = itsToxicity
 
 
-type Cactus = 
-    inherit Plant
+type Animal(itsName:string, itsAge:int, itsSpeed:int, itsAgility:int) =
+    inherit Creature(itsName, itsAge)
 
-    val toxic : bool
+    let speed = itsSpeed
+    let agility = itsAgility
 
-type Giraffe =
-    inherit Animal
+    member this.Speed = speed
+    member this.Agility = agility
 
-    val mutable caught : bool
 
-    member this.Caught() = this.caught <- true
-    member this.Free() = this.caught <- false
+type Cactus(itsToxicity:bool) = 
+    inherit Plant("Cactus", 0, itsToxicity)
 
-    member x.Eat (y : Cactus) = if (y.toxic) then x.Die()
+    member this.Toxic = itsToxicity
+
+//type PoisonousCactus() =
+  //  inherit Plant("Cactus", 0, true)
+
+type Giraffe(itsSpeed, itsAgility) =
+    inherit Animal("Giraffe", 0, itsSpeed, itsAgility)
+
+    let mutable caught = false
+    
+    member this.IsCaught = caught
+    member this.Caught() = caught <- true
+    member this.Free() = caught <- false
+
+    member x.Eat (y : Cactus) = if (y.Toxic) then x.Die()
                                              else y.Die()
 
     
 
-type Tiger = 
-    inherit Animal
+type Tiger(itsSpeed, itsAgility) = 
+    inherit Animal("Tiger", 0, itsSpeed, itsAgility)
 
     member x.Catch (y : Giraffe) =
-        if (x.speed > y.speed) then y.Caught()
+        if (x.Speed > y.Speed) then y.Caught()
 
-    member x.Eat (y : Giraffe) = if (x.agility > y.agility) && (y.caught) then y.Die()
-                                                                          else y.Free()      
+    member x.Eat (y : Giraffe) = if (x.Agility > y.Agility) && (y.IsCaught) then y.Die()
+                                                                            else y.Free()
