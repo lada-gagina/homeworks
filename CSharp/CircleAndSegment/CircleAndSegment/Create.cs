67d59ded7bd4725173d8040bc397bb429181531a
Lada Gagina <l4.swallow@gmail.com>
2013-11-05 10:21:37 +0400

    added CSharp folder

---
+++ CSharp/CircleAndSegment/CircleAndSegment/Create.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using Lib;
+
+namespace CircleAndSegment
+{
+    public class Create
+    {
+        int x;
+        int y;
+        int r;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+
+        static Random rnd = new Random();
+
+        public Create(int x = 0, int y = 0, int r = 0, int x1 = 0, int y1 = 0, int x2 = 0, int y2 = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.r = r;
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public void CreateRandom()
+        {
+            this.r = rnd.Next(10, 65);
+            this.x = rnd.Next(r, 200 - r);
+            this.y = rnd.Next(r, 200 - r);
+            this.x1 = rnd.Next(0, 200);
+            this.y1 = rnd.Next(0, 200);
+            this.x2 = rnd.Next(0, 200);
+            this.y2 = rnd.Next(0, 200);
+        }
+
+        public bool Intersect()
+        {
+            return Geom.Intersect(x, y, r, x1, y1, x2, y2);
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            graphics.DrawEllipse(new Pen(Color.Aquamarine, 4), x - r, y - r, 2 * r, 2 * r);
+            graphics.DrawLine(new Pen(Color.Black, 4), x1, y1, x2, y2);
+        }
+    }
+}
---
+++ CSharp/CircleAndSegment/CircleAndSegment/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace CircleAndSegment
+{
+    partial class Shell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.ForeColor = System.Drawing.Color.Coral;
+            this.button1.Location = new System.Drawing.Point(12, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generation button";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(37, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Press the Generation button";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Shell
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 314);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Shell";
+            this.Text = "Intersect Circle and Segment";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
