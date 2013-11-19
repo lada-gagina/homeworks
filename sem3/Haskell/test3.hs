aff615c7685ee9235bce4b1dc6dee957d146bd48
Lada <lada.gagina@gmail.com>
2013-11-19 17:54:51 +0400

    modified test3

--- sem3/Haskell/test3.hs
+++ sem3/Haskell/test3.hs
@@ -5,28 +5,34 @@
 
 
 -------------2---------------------------
-p1 = ('A', 'T')
-p2 = ('G', 'C')
+--p1 = ('A', 'T')
+--p2 = ('G', 'C')
 
-data Pair = P (Char, Char) deriving Show
-data Base = A | T | G | C deriving Show 
+--data Pair = P (Char, Char) deriving Show
+data Base = A | T | G | C deriving (Show, Eq) 
 
 
-pairs = [P c | c <- zip ['A', 'G', 'T', 'C'] ['T', 'C', 'A', 'G']]
+--pairs = [P c | c <- zip ['A', 'G', 'T', 'C'] ['T', 'C', 'A', 'G']]
 
 
 complement [] = []
 complement (x : xs)
-	| x == 'A' = 'T' : (complement xs)
-	| x == 'T' = 'A' : (complement xs)
-	| x == 'G' = 'C' : (complement xs)
-	| x == 'C' = 'G' : (complement xs)
-	| True = "" ++ (complement xs)
+	| x == A = T : (complement xs)
+	| x == T = A : (complement xs)
+	| x == G = C : (complement xs)
+	| x == C = G : (complement xs)
 
 
 -------------3-------------------
-f :: Char -> Char -> Char -> Integer
-replacement f [] = []
-replacement f (x1 : []) = []
-replacement f (x1 : x2 : []) = []
-replacement f (x1 : x2 : x3 : xs) = (f x1 x2 x3) : (replacement xs)
+--f :: Char -> Char -> Char -> Integer
+
+f _ _ _ = Just 1
+
+replacement f (x1 : t@(x2 : x3 : xs)) =
+	case f x1 x2 x3 of 
+		Just x -> x : (replacement f xs)
+		_ -> replacement f t
+replacement f _ = []
+
+-------------4--------------------
+
