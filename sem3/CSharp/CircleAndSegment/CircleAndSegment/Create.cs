4ccaf30861b55b300a5ab76bd553c64454501f7f
Lada <lada.gagina@gmail.com>
2013-11-25 04:14:33 +0400

    modified CircleAndSegment

--- sem3/CSharp/CircleAndSegment/CircleAndSegment/Create.cs
+++ sem3/CSharp/CircleAndSegment/CircleAndSegment/Create.cs
@@ -7,49 +7,40 @@ using Lib;
 
 namespace CircleAndSegment
 {
-    public class Create
+    public class Figures
     {
-        int x;
-        int y;
-        int r;
-        int x1;
-        int y1;
-        int x2;
-        int y2;
+        public Point circleCenter;
+        public int radius;
+        public Point lineSegment1;
+        public Point lineSegment2;
+
 
         static Random rnd = new Random();
 
-        public Create(int x = 0, int y = 0, int r = 0, int x1 = 0, int y1 = 0, int x2 = 0, int y2 = 0)
+        public Figures()
         {
-            this.x = x;
-            this.y = y;
-            this.r = r;
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
+            this.circleCenter = new Point();
+            this.lineSegment1 = new Point();
+            this.lineSegment2 = new Point();
         }
 
         public void CreateRandom()
         {
-            this.r = rnd.Next(10, 65);
-            this.x = rnd.Next(r, 200 - r);
-            this.y = rnd.Next(r, 200 - r);
-            this.x1 = rnd.Next(0, 200);
-            this.y1 = rnd.Next(0, 200);
-            this.x2 = rnd.Next(0, 200);
-            this.y2 = rnd.Next(0, 200);
+            this.radius = rnd.Next(10, 65);
+            this.circleCenter = new Point(rnd.Next(radius, 200 - radius), rnd.Next(radius, 200 - radius));
+            this.lineSegment1 = new Point(rnd.Next(0, 200), rnd.Next(0, 200));
+            this.lineSegment2 = new Point(rnd.Next(0, 200), rnd.Next(0, 200));
         }
 
-        public bool Intersect()
+        public bool AreIntersecting()
         {
-            return Geom.Intersect(x, y, r, x1, y1, x2, y2);
+            return Geom.AreIntersecting(circleCenter, radius, lineSegment1, lineSegment2);
         }
 
         public void Draw(Graphics graphics)
         {
-            graphics.DrawEllipse(new Pen(Color.Aquamarine, 4), x - r, y - r, 2 * r, 2 * r);
-            graphics.DrawLine(new Pen(Color.Black, 4), x1, y1, x2, y2);
+            graphics.DrawEllipse(new Pen(Color.Aquamarine, 4), circleCenter.X, circleCenter.Y, 2 * radius, 2 * radius);
+            graphics.DrawLine(new Pen(Color.Black, 4), lineSegment1.X, lineSegment1.Y, lineSegment2.X, lineSegment2.Y);
         }
     }
 }
